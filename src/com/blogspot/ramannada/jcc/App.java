63b166b7a9e3c774dbf54f5a5a74d28fda656f5a
ramannada <labibmuhajir@yahoo.com>
2017-12-10 15:17:57 +0700

    first init

--- src/com/blogspot/ramannada/jcc/App.java
+++ src/com/blogspot/ramannada/jcc/App.java
@@ -1,12 +1,11 @@
 package com.blogspot.ramannada.jcc;
 
-import com.blogspot.ramannada.jcc.utils.MariaDB;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        MariaDB.getMariaDbPoolDataSource();
+
     }
 }
